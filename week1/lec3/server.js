42a892699c40715060af1b71e4456307b78ef772
MayankDalal19 <iammayank19042007@gmail.com>
2026-08-11 10:55:16 +0530

    first commit

---
+++ week1/lec3/server.js
@@ -0,0 +1,8 @@
+// const math = require('.\math.js')
+// const os = require('os');
+// const express = require('express')
+
+import {add,subtract} from './math.js';
+
+console.log(add(2,3));
+console.log(subtract(5,1));
